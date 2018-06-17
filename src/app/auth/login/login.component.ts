fbde0049230da728f87220e759e20373f128e8b6
taras-d <taras.datsenko@gmail.com>
2018-06-17 21:03:03 +0300

    Login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'v-login',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean;
 
-  ngOnInit() {
+  message: { type: string, text: string };
+
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+
+  }
+
+  buildForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  onLogin(): void {
+    console.log(this.loginForm.value);
+
+    this.message = null;
+    this.loading = true;
+    
+    setTimeout(() => {
+      this.message = { type: 'negative', text: 'Incorrect email or password' };
+      this.loading = false;
+    }, 600);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SuiMessageModule } from 'ng2-semantic-ui';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    SuiMessageModule
   ],
   exports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    SuiMessageModule
   ],
   declarations: []
 })
