5e8e32d4100a89da34bb8f8d8fccc44e4d654909
taras-d <taras.datsenko@gmail.com>
2018-06-21 08:01:22 +0300

    Words table text overflow effect

--- src/app/core/utils/decorateWord.ts
+++ src/app/core/utils/decorateWord.ts
@@ -1,5 +1,8 @@
+import * as moment from 'moment';
+
 export const decorateWord = word => {
   if (word) {
+    word.date = moment(word.createdAt).fromNow();
     word.googleTranslateLink = `https://translate.google.com/?#en/auto/${word.text}`;
     word.googleImagesLink = `https://www.google.com/search?tbm=isch&q=${word.text}`;
   }
