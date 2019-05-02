3fb458ab067ea20812b27071409b844a05808497
taras-d <taras.datsenko@gmail.com>
2019-05-02 16:37:47 +0300

    Update word edit

--- src/app/vocabulary/word-edit/word-edit.component.ts
+++ src/app/vocabulary/word-edit/word-edit.component.ts
@@ -1,115 +1,51 @@
-import { Component, OnInit, OnDestroy, ViewChild, Output, EventEmitter, TemplateRef, ElementRef } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { SuiModalService, TemplateModalConfig, SuiActiveModal } from 'ng2-semantic-ui';
-import * as _ from 'lodash';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { NzNotificationService } from 'ng-zorro-antd';
 
-import { notEmpty } from '@core/validators';
-import { ObserverManager, getErrorMsg } from '@core/utils';
-import { AppService, WordsService } from '@core/services';
+import { BaseComponent, getErrorMsg } from '@core/utils';
+import { WordsService } from '@core/services';
 
 @Component({
   selector: 'v-word-edit',
   templateUrl: './word-edit.component.html',
   styleUrls: ['./word-edit.component.less']
 })
-export class WordEditComponent implements OnInit, OnDestroy {
-
-  @ViewChild('modalTemplate') modalTemplateRef: TemplateRef<any>;
-  @ViewChild('form') formRef: ElementRef;
-
+export class WordEditComponent extends BaseComponent {
   @Output() complete = new EventEmitter();
 
   loading: boolean;
-
+  visible: boolean;
   word: any;
-  wordForm: FormGroup;
-
-  modal: SuiActiveModal<any, any, any>;
-
-  om: ObserverManager;
 
   constructor(
-    private fb: FormBuilder,
-    private suiModalService: SuiModalService,
-    private appService: AppService,
+    private notificationService: NzNotificationService,
     private wordsService: WordsService
   ) {
-    this.om = new ObserverManager({
-
-      updateWord: {
-        create: () => {
-          this.loading = true;
-          return this.wordsService.updateWord(this.word._id, this.wordForm.value);
-        },
-        next: () => {
-          this.loading = false;
-          this.modal.approve(null);
-          this.complete.emit();
-        }
-      }
-
-    }, {
-
-      error: (name, err) => {
-        this.appService.pushMessage({
-          header: 'Error', text: getErrorMsg(err), type: 'error'
-        });
-        this.loading = false;
-      }
-
-    });
+    super();
   }
 
-  ngOnInit(): void {
-
-  }
-
-  ngOnDestroy(): void {
-    this.om.unsubAll();
-  }
-
-  buildForm(data: any): void {
-    this.wordForm = this.fb.group({
-      text: [data.text, notEmpty],
-      translation: [data.translation]
+  updateWord(): void {
+    this.loading = true;
+    this.wordsService.updateWord(this.word._id, this.word).pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.loading = false;
+      this.visible = false;
+      this.complete.emit();
+    }, err => {
+      this.notificationService.error('Error', getErrorMsg(err));
+      this.loading = false;
     });
   }
 
   open(word: any): void {
-    this.word = _.cloneDeep(word);
-
-    this.buildForm(word);
-
-    const config = new TemplateModalConfig(this.modalTemplateRef);
-    config.isInverted = true;
-    config.size = 'tiny';
-    config.mustScroll = true;
-
-    this.modal = this.suiModalService.open(config);
-    this.modal.onDeny(() => {
-      this.word = this.wordForm = null;
-      this.om.unsubAll();
-    });
-
-    this.focusTextField();
+    this.word = Object.assign({}, word);
+    this.visible = true;
   }
 
-  onSave(): void {
-    this.om.invoke('updateWord');
+  close(): void {
+    this.word = null;
+    this.loading = false;
+    this.visible = false;
   }
-
-  onCancel(): void {
-    this.modal.deny(null);
-  }
-
-  focusTextField(): void {
-    setTimeout(() => {
-      const form = this.formRef.nativeElement as HTMLFormElement;
-      const field = form.querySelector('.field:first-child textarea') as HTMLTextAreaElement;
-      if (field) {
-        field.focus();
-      }
-    }, 100);
-  }
-
 }
