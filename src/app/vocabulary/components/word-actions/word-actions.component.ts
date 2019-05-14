1e7f906273c45cd3b5d06801f567ee06e7bcada1
taras-d <taras.datsenko@gmail.com>
2019-05-14 15:41:08 +0300

    Word actions dropdown

--- src/app/vocabulary/components/word-actions/word-actions.component.ts
+++ src/app/vocabulary/components/word-actions/word-actions.component.ts
@@ -1,12 +1,38 @@
-import { Component, Output, EventEmitter, Input, HostListener } from '@angular/core';
+import { Component, Output, EventEmitter, Input, HostListener, OnInit } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'v-word-actions',
   templateUrl: './word-actions.component.html',
   styleUrls: ['./word-actions.component.less']
 })
-export class WordActionsComponent {
+export class WordActionsComponent implements OnInit {
   @Input() word: any;
   @Output() edit = new EventEmitter();
   @Output() delete = new EventEmitter();
+
+  isSmallScreen: boolean;
+
+  constructor(private modalService: NzModalService) {
+
+  }
+
+  ngOnInit(): void {
+    this.detectScreen();
+  }
+
+  deleteConfirm(): void {
+    this.modalService.confirm({
+      nzTitle: 'Delete word',
+      nzContent: 'Are you sure?',
+      nzOkText: 'Yes',
+      nzCancelText: 'No',
+      nzOnOk: () => this.delete.emit()
+    });
+  }
+
+  @HostListener('window:resize')
+  private detectScreen(): void {
+    this.isSmallScreen = window.innerWidth <= 650;
+  }
 }
