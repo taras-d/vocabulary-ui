af9f89eeb499110e45ce8b090c7e48afe3440520
taras-d <taras.datsenko@gmail.com>
2019-05-29 16:56:31 +0300

    Fix error message

--- src/app/core/services/error.service.ts
+++ src/app/core/services/error.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { NzNotificationService } from 'ng-zorro-antd';
 
 import { environment } from '@env/environment';
@@ -9,11 +10,16 @@ export class ErrorService {
 
   }
 
-  handleError(err): void {
+  handleError(err: any): void {
     if (!environment.production) {
       console.warn(err);
     }
 
-    this.notificationService.error('Error', 'Service is unavailable. Please try again later.');
+    this.notificationService.error('Error', this.getErrorMessage(err));
+  }
+
+  private getErrorMessage(err: any): string {
+    return (err instanceof HttpErrorResponse && err.error && err.error.message) ?
+      err.error.message : 'Service is unavailable. Please try again later.';
   }
 }
