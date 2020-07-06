47904606e98e8145bfbf1561c15bb8f7c2cc30aa
taras-d <taras.datsenko@gmail.com>
2020-07-06 09:38:57 +0300

    Update to angular 10

--- src/app/shared/components/base/base.component.ts
+++ src/app/shared/components/base/base.component.ts
@@ -1,7 +1,8 @@
-import { OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ClrLoadingState } from '@clr/angular';
 
+@Component({ selector: 'v-base', template: '' })
 export class BaseComponent implements OnDestroy {
   loading: boolean | ClrLoadingState;
   message: { type: string; text: string };
