4d7ded0e5d11f5667a0c7558de1a4de6fab7d023
taras-d <taras.datsenko@gmail.com>
2020-07-18 20:23:24 +0300

    Update icons

--- src/app/shared/components/page-layout/page-layout.component.ts
+++ src/app/shared/components/page-layout/page-layout.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '@core/services/auth.service';
+import { User } from '@core/models/auth';
 
 @Component({
   selector: 'v-page-layout',
   templateUrl: './page-layout.component.html',
   styleUrls: ['./page-layout.component.less']
 })
-export class PageLayoutComponent {
+export class PageLayoutComponent implements OnInit {
+  user: User;
+
   constructor(
     private authService: AuthService
   ) {
 
+  }
+
+  ngOnInit(): void {
+    this.user = this.authService.user;
   }
 
   logout(): void {
